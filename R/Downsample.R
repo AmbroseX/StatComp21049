c78ab1078fa416bf4810f0fb5b6de1ed9244867e
****# <earth@mail.ustc.edu.cn>
2021-12-19 23:49:49 +0800

    homework finish

---
+++ R/Downsample.R
@@ -0,0 +1,17 @@
+#' @title A function to downsample the raw data
+#' @description Downsample the time sequence and data
+#' @param timesqe a vector with T*1 size
+#' the unit is (s)
+#' @param dataseq a matrix with T*N size
+#' @param dft downsample to dfz (Hz)
+#' @param n to control the wave  filter
+#' @return a number, the dimension of the matrix X
+#' @examples
+#' \dontrun{
+#' X <- Plot_Matrix_Dimension()
+#' }
+#' @export
+Downsample <- function(timeseq, dataseq, dft){
+
+
+}
