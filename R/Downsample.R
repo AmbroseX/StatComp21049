acfffe9839f1b822612e30af2893b8e79c89ed48
****# <earth@mail.ustc.edu.cn>
2021-12-20 00:34:51 +0800

    Downsample

--- R/Downsample.R
+++ R/Downsample.R
@@ -1,17 +1,44 @@
 #' @title A function to downsample the raw data
 #' @description Downsample the time sequence and data
-#' @param timesqe a vector with T*1 size
+#' @param timeseq a vector with T*1 size
 #' the unit is (s)
 #' @param dataseq a matrix with T*N size
 #' @param dft downsample to dfz (Hz)
-#' @param n to control the wave  filter
-#' @return a number, the dimension of the matrix X
+#' @return a list,contain $time_downsampled, $data_downsampled
 #' @examples
 #' \dontrun{
-#' X <- Plot_Matrix_Dimension()
+#' data("curve_data","time_Elapsed")
+#' X <- Plot_Matrix_Dimension(curve_data,time_Elapsed,16)
+#' time_downsample
 #' }
 #' @export
-Downsample <- function(timeseq, dataseq, dft){
+Downsample <- function(timeseq, dataseq, dft=16){
+  sizedata <- dim(dataseq)
+  m <- sizedata[1]
+  n <- sizedata[2]
 
 
+  dt <- diff(timeseq)
+  meanft <- mean(1./dt)
+  rfolde <- round(meanft/dft)
+
+
+  if(rfolde > 1){
+    T <- ceiling(m/rfolde)
+    time_downsampled <- numeric(T)
+    data_downsampled <- matrix(0,T,n)
+    for(i in 1:T){
+      time_downsampled[i] <- timeseq[4*i-3]
+      for(j in 1:n){
+        data_downsampled[i,j] <- dataseq[4*i-3,j]
+      }
+    }
+  }
+  else{
+    time_downsampled <- timeseq
+    data_downsampled <- dataseq
+
+  }
+  rec <- list(time_downsampled = time_downsampled,data_downsampled=data_downsampled)
+  return(rec)
 }
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,7 +3,10 @@
 # batch_deseq ------------------------------------------------------------------
 
 curve_data <- read.csv("data-raw/curve_data.csv",header = FALSE)
+curve_data <- data.matrix(curve_data)  # 将dataframe转换为矩阵
+
 time_Elapsed <- read.csv("data-raw/TimeElapsed.csv",header = FALSE)
+time_Elapsed <- time_Elapsed$V1
 
 usethis::use_data(curve_data, compress = "xz", overwrite = TRUE)
 usethis::use_data(time_Elapsed, compress = "xz", overwrite = TRUE)
