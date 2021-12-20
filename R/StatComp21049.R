cff0adfb9ca72814eb91d9bc895e103bc17fcb43
****# <earth@mail.ustc.edu.cn>
2021-12-21 00:04:01 +0800

    final

--- R/StatComp21049.R
+++ R/StatComp21049.R
@@ -1,19 +1,24 @@
 #' StatComp21049: A package for Calculate the phase space projection.
 #'
+#' StatComp21049 is a R package developed to present the homework of statistic
+#' computing course by author StatComp21049 and reconstruct the Phase space for worm behavior.
+#'
 #' The StatComp21049 package provides three categories of important functions:
 #' Embedding_delay,Downsample,Get_Matrix_Eigens,Get_Matrix_Dimension
 #'
-#' @section StatComp21049 functions:
-#' \code{\link{Embedding_delay}}
-#' \code{\link{Downsample}}
-#' \code{\link{Get_Matrix_Eigens}}
-#' \code{\link{Get_Matrix_Dimension}}
-#' StatComp21049 is a R package developed to present the homework of statistic
-#' computing course by author StatComp21049 and reconstruct the Phase space for worm behavior.
 #' You can You can visit the online webpage to view the help documentation from
-#' \url{https://rongkangxiong.github.io/StatComp21049} The online documentation
-#' You can visit my blog \href{https://neuralife.cn}
-#' You can email me from \email{earth@@mail.ustc.edu.cn}
+#' \url{https://rongkangxiong.github.io/StatComp21049}
+#'
+#' @section StatComp21049 functions:
+#' \code{\link{Embedding_delay}} : Function to Embedding and delay the matrix after PCA projection.
+#' \code{\link{Downsample}} : Function to Downsample data.
+#' \code{\link{Get_Matrix_Eigens}} : Function to get eigenvalue and eigenvector.
+#' \code{\link{Get_Matrix_Dimension}} : Get the matrix real dimension.
+#'
+#' You can visit my blog:
+#' \url{https://neuralife.cn}.
+#'
+#' You can email me from \email{earth@@mail.ustc.edu.cn}.
 #'
 #' @docType package
 #' @name StatComp21049
